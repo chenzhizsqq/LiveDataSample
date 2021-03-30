639d314878d34632c7a259e155ff7570d8991b09
chenzhi <chenzhi@qq.com>
2021-03-30 10:32:52 +0900

    给每个步骤都详细说明

--- app/src/main/java/com/test/livedatasample/MainActivity.java
+++ app/src/main/java/com/test/livedatasample/MainActivity.java
@@ -9,17 +9,26 @@ import com.test.livedatasample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    //下面的ActivityMainBinding，就是activity_main.xml的对应类。这个Android固定的格式
+    //如果要互相绑定好，这里就需要用上下面的格式。例如activity_main.xml就等于ActivityMainBinding
+    //其实就是ActivityMain + Binding，这个官方固定的
     private ActivityMainBinding binding;
 
+    //新建一个关于LiveData的类
     private MainViewModel viewModel = new MainViewModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //在这里就是说明了这个binding是对应着R.layout.activity_main，那么就可以在R.layout.activity_main中使用
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        // ここでLifecycleOwnerセットしないとLiveDataが反映されない
+        //setLifecycleOwner     设置生命周期所有者
+        //在这里一定要用LifecycleOwner设定，才能够说明用上了LiveData
         binding.setLifecycleOwner(this);
+
+        //在这里一定要用setViewModel设定，才能够说明对应了MainViewModel
         binding.setViewModel(viewModel);
     }
 }
--- app/src/main/java/com/test/livedatasample/MainViewModel.java
+++ app/src/main/java/com/test/livedatasample/MainViewModel.java
@@ -8,15 +8,26 @@ import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
 
+    //MutableLiveData   可变实时数据
+    //在这里定义一个变量text，说明这个变量可以在给定义了setLifecycleOwner后的ViewModel的Activity使用。
     private MutableLiveData<String> text;
 
+    //这里，就给activity_main.xml的android:text="@{viewModel.text}"添加了相应的函数。
+    //只需要写上LiveData<String> getText()就可以了。不用在像之前的写上view.findViewById(R.id.***)的样式。
+    //要用上MutableLiveData与xml页面的viewModel.text对应。就一定要用上LiveData<String> getText()。
     public LiveData<String> getText() {
+
+        //而且getText()函数中，一定下加上if (text == null) {text = new MutableLiveData<>();}这种如果变量没有了，就要再创建变量。
+        //其实它的作用就是怕次变量已经给Android清除掉了，让变量再创建。
+        //我也确实多次试过某变量总是找不到的经历，也猜想到是被android系统自己清除掉了。
         if (text == null) {
             text = new MutableLiveData<>();
         }
+
         return text;
     }
 
+    //而这里就是给activity_main.xml的android:onClick="@{viewModel::onClick}"添加了对应的函数。
     public void onClick(View view) {
         text.setValue("time: " + System.currentTimeMillis());
     }
