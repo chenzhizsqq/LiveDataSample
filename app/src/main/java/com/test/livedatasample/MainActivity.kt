662d02032e2c96e690985254099ac7b9c9525765
chenzhi <chenzhi@qq.com>
2021-03-30 11:31:02 +0900

    转为kotlin

--- app/src/main/java/com/test/livedatasample/MainActivity.kt
+++ app/src/main/java/com/test/livedatasample/MainActivity.kt
@@ -1,34 +1,31 @@
-package com.test.livedatasample;
+package com.test.livedatasample
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.test.livedatasample.databinding.ActivityMainBinding
 
-import android.os.Bundle;
-
-import com.test.livedatasample.databinding.ActivityMainBinding;
-
-public class MainActivity extends AppCompatActivity {
-
-    //下面的ActivityMainBinding，就是activity_main.xml的对应类。这个Android固定的格式
-    //如果要互相绑定好，这里就需要用上下面的格式。例如activity_main.xml就等于ActivityMainBinding
-    //其实就是ActivityMain + Binding，这个官方固定的
-    private ActivityMainBinding binding;
+class MainActivity : AppCompatActivity() {
 
     //新建一个关于LiveData的类
-    private MainViewModel viewModel = new MainViewModel();
+    private val viewModel = MainViewModel()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        //下面的ActivityMainBinding，就是activity_main.xml的对应类。这个Android固定的格式
+        //如果要互相绑定好，这里就需要用上下面的格式。例如activity_main.xml就等于ActivityMainBinding
+        //其实就是ActivityMain + Binding，这个官方固定的
+        var binding: ActivityMainBinding? = null
+
 
         //在这里就是说明了这个binding是对应着R.layout.activity_main，那么就可以在R.layout.activity_main中使用
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         //setLifecycleOwner     设置生命周期所有者
         //在这里一定要用LifecycleOwner设定，才能够说明用上了LiveData
-        binding.setLifecycleOwner(this);
+        binding.lifecycleOwner = this
 
         //在这里一定要用setViewModel设定，才能够说明对应了MainViewModel
-        binding.setViewModel(viewModel);
+        binding.viewModel = viewModel
     }
 }
--- app/src/main/java/com/test/livedatasample/MainViewModel.kt
+++ app/src/main/java/com/test/livedatasample/MainViewModel.kt
@@ -1,34 +1,31 @@
-package com.test.livedatasample;
+package com.test.livedatasample
 
-import android.view.View;
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-
+class MainViewModel : ViewModel() {
     //MutableLiveData   可变实时数据
     //在这里定义一个变量text，说明这个变量可以在给定义了setLifecycleOwner后的ViewModel的Activity使用。
-    private MutableLiveData<String> text;
+    private var text: MutableLiveData<String>? = null
 
     //这里，就给activity_main.xml的android:text="@{viewModel.text}"添加了相应的函数。
     //只需要写上LiveData<String> getText()就可以了。不用在像之前的写上view.findViewById(R.id.***)的样式。
     //要用上MutableLiveData与xml页面的viewModel.text对应。就一定要用上LiveData<String> getText()。
-    public LiveData<String> getText() {
+    fun getText(): LiveData<String> {
 
         //而且getText()函数中，一定下加上if (text == null) {text = new MutableLiveData<>();}这种如果变量没有了，就要再创建变量。
         //其实它的作用就是怕次变量已经给Android清除掉了，让变量再创建。
         //我也确实多次试过某变量总是找不到的经历，也猜想到是被android系统自己清除掉了。
         if (text == null) {
-            text = new MutableLiveData<>();
+            text = MutableLiveData()
         }
-
-        return text;
+        return text!!
     }
 
     //而这里就是给activity_main.xml的android:onClick="@{viewModel::onClick}"添加了对应的函数。
-    public void onClick(View view) {
-        text.setValue("time: " + System.currentTimeMillis());
+    fun onClick(view: View?) {
+        text!!.value = "time: " + System.currentTimeMillis()
     }
 }
